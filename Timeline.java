abdee732bb3b71586a482dd5e22578f4011428ff
Andrew Hard <hardandrew1@gmail.com>
2014-10-17 17:51:53 -0500

    Making case sensitive user

--- Timeline.java
+++ Timeline.java
@@ -1,4 +1,27 @@
+///////////////////////////////////////////////////////////////////////////////
+// 
+// Main Class File:  Twitter.java 
+// File:             Timeline.java
+// Semester:         CS367 Fall 2014
+//
+// Author:           Andrew Hard 
+// Email:            hard@wisc.edu
+// CS Login:         hard
+// Lecturer's Name:  Jim Skrentny 
+// Lab Section:      LEC-002 (77632)
+//
+///////////////////////////////////////////////////////////////////////////////
+//
+// Pair Partner:     Wayne Chew
+// Email:            mchew2@wisc.edu
+// CS Login:         mchew
+// Lecturer's Name:  Jim Skrentny
+// Lab Section:      LEC-001 (77631)
+//
+///////////////////////////////////////////////////////////////////////////////
+
 import java.util.*;
+
 /**
  * The Timeline class uses SimpleLinkedList to build a time ordered list of 
  * following tweets. Tweets with smaller Time fields should come earlier in the list.
@@ -66,7 +89,7 @@ class Timeline{
 	    // careful when removing to not skip over a tweet...
 	    while (tlIndex < tlList.size()) {
 	
-		if (tlList.get(tlIndex).getUser().toLowerCase().equals(user.toLowerCase())) {
+		if (tlList.get(tlIndex).getUser().equals(user)) {
 		    tlList.remove(tlIndex);
 		}
 		
