c81f409f0dc18244181c6989c3de3c2eacee1703
Andrew Hard <hardandrew1@gmail.com>
2014-10-18 13:00:16 -0500

    Adding Twitter comments

--- Twitter.java
+++ Twitter.java
@@ -22,8 +22,6 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-// TODO *** add comments as specified in the commenting guide ***
-
 import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -35,7 +33,7 @@ import java.io.FileNotFoundException;
  * associated with the followed users. 
  */
 
-public class  Twitter{
+public class Twitter{
 
     /** 
      * Search for a String in a list of Strings.
@@ -71,21 +69,23 @@ public class  Twitter{
 	
 	// Create instances of important variables:
 	List<List<Tweet>> tweets = new ArrayList<List<Tweet>>();//contains list of tweets sort by user
-    List<Tweet> userTweets;//contain tweets
-    Timeline followTimeline = new Timeline();//contains only the tweet the user is following
-	List<String> currUser = new ArrayList<String>();//contains all user
-    List<String> followUser = new ArrayList<String>();//contains only followed user
-    String user;
+	List<Tweet> userTweets;//contain tweets
 	
-	// Loop over input files (users):
+	Timeline followTimeline = new Timeline();//contains only the tweet the user is following
+	List<String> currUser = new ArrayList<String>();//contains all user
+	List<String> followUser = new ArrayList<String>();//contains only followed user
+	String user;
+	
+	
+	// Loop over input files (one per user):
 	for (int i = 0; i < args.length; i++) {
 	    File dataFile = new File(args[i]);
 	    
 	    //currUser.args[i].replaceAll(".txt","");
-        user = args[i].replace(".txt","");
+	    user = args[i].replace(".txt","");
 	    currUser.add(user);
-        followUser.add(user);
-
+	    followUser.add(user);
+	    
 	    // Check whether input file exists and is readable: 
 	    if (!dataFile.exists()) {
 		System.out.println("Error: cannot access input file");
@@ -97,7 +97,7 @@ public class  Twitter{
 	    
 	    // Loop through the current input file:
 	    // Each line is one tweet
-        userTweets = new ArrayList<Tweet>();//store a list of tweets
+	    userTweets = new ArrayList<Tweet>();//store a list of tweets
 	    while (inputFile.hasNext()) {
 		
 		String currLine = inputFile.nextLine();
@@ -108,15 +108,16 @@ public class  Twitter{
 		userTweets.add(new Tweet(Integer.parseInt(splitLine[0]), splitLine[1], currUser.get(i)));
 	    }
 	    inputFile.close();
-        tweets.add(userTweets);//store list of tweets by user
+	    tweets.add(userTweets);//list storing lists of tweets by user
 	}
-    
-	   for(int i = 0; i < tweets.size(); i++){
+	
+	for (int i = 0; i < tweets.size(); i++) {
             followTimeline.add(tweets.get(i));
-       }
+	}
 	
 	
-        Scanner stdin = new Scanner(System.in);  //for console input
+	// Initialize console input:
+        Scanner stdin = new Scanner(System.in);
 	
         boolean done = false;
         while (!done) {
@@ -126,105 +127,128 @@ public class  Twitter{
             //only do something if the user enters at least one character
             if (input.length() > 0) {
                 String[] commands = input.split("[ ]+");//split on space
-               
-		switch(commands[0]) {
 		
-		case "list":
-            if(commands.length == 1){
-                System.out.println("Invalid command");
-                break;
-            }
-            switch(commands[1]){
-                case "users":
-                    for(int i = 0; i < currUser.size(); i++){
-                        System.out.println(currUser.get(i));
-                    }
-                    break;//case users
-
-                case "following":
-                    for(int i = 0; i < followUser.size(); i++){
-                        System.out.println(followUser.get(i));
-                    }
-                    break;
-            }
-            
-		    break;
-		case "follow":
-            user = commands[1];
-            if(Compare(currUser,user)){
-                if(!Compare(followUser,user)){
-                    followUser.add(user);
-                    for(int i = 0; i < tweets.size(); i++){
-                       if(tweets.get(i).get(0).getUser().equals(user)){
-                            followTimeline.add(tweets.get(i));
-                        }//add tweets to timeline
-                    }
-                }//check whether user already followed
-                else{
-                    System.out.println("Warning: User already followed");
-                }
-            }//check whether user exists
-            else{
-                System.out.println("Invalid user");
-            }
-            
-		    break;
-
-		case "unfollow":
-            user = commands[1];
-            if(Compare(currUser,user)){
-                if(Compare(followUser,user)){
-                    followUser.remove(user);
-                    followTimeline.remove(user);//remove tweets from timeline
-                }//check whether user already unfollowed
-                else{
-                    System.out.println("Warning: User not followed");
-                }
-            }//check whether user exists
-            else{
-                System.out.println("Invalid user");
-            }
-		    break;
-
-		case "search":
-            String string = commands[1];
-            followTimeline.search(string).print();
-
-		    break;
+		switch (commands[0]) {
 		    
-		case "print":
-            if(commands.length == 1){
-                followTimeline.print();
-            }
-            else if(commands.length == 2){
-                followTimeline.print(Integer.parseInt(commands[1]));
-            }
-
-
-            /*
-		    if (commands[1].trim().length() > 0) {
-			twitTimeline.print();
+		    
+		    
+		case "list": // Case 1: list all or following users
+		    if (commands.length == 1) {
+			System.out.println("Invalid command");
+			break;
 		    }
-		    else {
-			// this casting might throw some exception...
-			int currTime = valueOf(commands[1]);
-			if (currTime < 0) {
-			    throw new IllegalArgumentException();
+		    switch (commands[1]) {
+		    case "users":
+			for (int i = 0; i < currUser.size(); i++) {
+			    System.out.println(currUser.get(i));
 			}
-			twitTimeline.print(currTime);
+			break;//case users
+			
+		    case "following":
+			for (int i = 0; i < followUser.size(); i++) {
+			    System.out.println(followUser.get(i));
+			}
+			break;
 		    }
-            */
+		    break;
+
+		    
+		    
+		case "follow": // Follow an existing user
+		    user = commands[1];
+		  
+		    // First check that user exists:
+		    if (Compare(currUser,user)) {
+
+			// If they are not yet followed, follow
+			if (!Compare(followUser, user)) {
+			    followUser.add(user);
+			    
+			    for (int i = 0; i < tweets.size(); i++) {
+				if (tweets.get(i).get(0).getUser().equals(user)) {
+				    followTimeline.add(tweets.get(i));
+				}
+			    }
+			}
+			
+			else {// If user already followed:
+			    System.out.println("Warning: User already followed");
+			}
+		    }
+		    else {// If user does not exist:
+			System.out.println("Invalid user");
+		    }
+		    
 		    break;
 		    
-		case "quit":
+		    
+		    
+		case "unfollow": // Unfollow an existing user
+		    user = commands[1];
+		    
+		    // First check that user exists:
+		    if (Compare(currUser,user)) {
+			
+			// If they are already followed, unfollow:
+			if (Compare(followUser, user)) {
+			    followUser.remove(user);
+			    followTimeline.remove(user);//remove tweets from timeline
+			}
+			else {// If user already not followed:
+			    System.out.println("Warning: User not followed");
+			}
+		    }
+		    else {// If user does not exist:
+			System.out.println("Invalid user");
+		    }
+		    break;
+		    
+		    
+		    
+		case "search": // Search for tweets that contain a string
+		    String string = commands[1];
+		    followTimeline.search(string).print();
+		    
+		    break;
+		    
+		    
+		    
+		case "print": // Prints the timeline
+		    if (commands.length == 1) {// print entire timeline
+			followTimeline.print();
+		    }
+		    else if(commands.length == 2){//print after given time
+			followTimeline.print(Integer.parseInt(commands[1]));
+		    }
+		    
+		    /*
+		      if (commands[1].trim().length() > 0) {
+		      twitTimeline.print();
+		      }
+		      else {
+		      // this casting might throw some exception...
+		      int currTime = valueOf(commands[1]);
+		      if (currTime < 0) {
+		      throw new IllegalArgumentException();
+		      }
+		      twitTimeline.print(currTime);
+		      }
+		    */
+		    
+		    break;
+		    
+		    
+		case "quit": // Exit the program
 		    done = true;
 		    System.out.println("exit");
 		    break;
 		    
-		default:  //a command with no argument
+		    
+		default: // A command with no argument
 		    System.out.println("Invalid Command");
 		    break;
                 }
+		
             } //end if
         } //end while
     } //end main
